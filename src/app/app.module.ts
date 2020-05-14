25d71df1a6962d37274bd5b9980d48eabd4892a7
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2020-05-14 20:41:53 +0300

    Add setup of elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TabsComponent
   ],
   imports: [
     BrowserModule

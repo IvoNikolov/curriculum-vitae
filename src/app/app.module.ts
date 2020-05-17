41daa42ed0f1b35fcaa4bcf8e9deaa3efbb50c14
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2020-05-17 15:05:14 +0300

    Fix for the collapsed hamburger menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TabsComponent } from './tabs/tabs.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    TabsComponent
+    TabsComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule

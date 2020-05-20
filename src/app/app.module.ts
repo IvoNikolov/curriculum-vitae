4b88e9aa83528424ca25c42a84fa54fe0f6b7b65
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2020-05-20 09:27:16 +0300

    Add second test page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TabsComponent } from './tabs/tabs.component';
 import { TestComponent } from './test/test.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     TabsComponent,
-    TestComponent
+    TestComponent,
+    Test2Component
   ],
   imports: [
     BrowserModule
